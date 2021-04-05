e631a402ad91be1e715a91c77afdf5afb520eb70
Riko Kami <orglary@gmail.com>
2021-04-05 02:13:58 -0300

    initial bot discord

---
+++ src/index.tsx
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+require("dotenv").config();
+
+const client = new Discord.Client();
+
+var expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+var regex = new RegExp(expression);
+
+client.on("message", (message) => {
+  if (message.author.bot) return;
+  const args = message.content.split(" ");
+  const command = args.shift().toLowerCase();
+
+  // TODO SELECT CHANNEL
+  if (message.channel.name === "🖇links" || message.channel.name === "botzada") {
+    console.table({
+      "Author:": message.author.username,
+      "Mensagem enviada:": message.content,
+    });
+
+    if (command.match(regex)) {
+      console.log("Successful match");
+    } else {
+      console.log("No match");
+      message.delete();
+    }
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
